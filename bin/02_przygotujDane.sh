9e0e946f2a23c6c9b12534c1502d6045288ceb93
Wlodek Bzyl <matwb@univ.gda.pl>
2018-03-02 16:26:21 +0100

    Update

--- bin/02_przygotujDane.sh
+++ bin/02_przygotujDane.sh
@@ -2,12 +2,12 @@
 
 folderPktAdr="pktadr"
 folderTymczasowy="temp"
-folderGeojson="geojson"
+folder_json="json"
 numerPliku=0
 mkdir -p $folderTymczasowy
-mkdir -p $folderGeojson
+mkdir -p $folder_json
 rm -f $folderTymczasowy/*
-rm -f $folderGeojson/*
+rm -f $folder_json/*
 
 shopt -s nullglob
 pliki=($folderPktAdr/*.xml)
@@ -16,13 +16,13 @@ pliki=($folderPktAdr/*.xml)
 for plikxml in "${pliki[@]}"
 do
   printf 'Przetwarzanie ogr2ogr: %s\n' "$plikxml"
-  plikjson=$(basename $plikxml).geojson
+  plikjson=$(basename $plikxml).json
   ogr2ogr -s_srs EPSG:2180 -t_srs EPSG:4326 -lco ENCODING=UTF-8 -f "GeoJSON" \
     -clipsrclayer "PRG_PunktAdresowy" \
     $folderTymczasowy/$plikjson $plikxml -skipfailures
   printf 'Przetwarzanie jq: %s\n' "$plikxml"
   jq --compact-output ".features[]" $folderTymczasowy/$plikjson \
-    > $folderGeojson/$plikjson
+    > $folder_json/$plikjson
   rm $folderTymczasowy/$plikjson
   numerPliku=$(($numerPliku + 1))
 done
